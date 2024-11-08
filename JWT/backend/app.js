21b242c3fd2cc9bb3da0f866ab4acfa9162f1c4a
Muriel Esneider <eesneider2118@gmail.com>
2024-11-08 17:26:45 -0500

    3

--- JWT/backend/app.js
+++ JWT/backend/app.js
@@ -7,19 +7,25 @@ const app = express();
 app.use(express.json()); //para parsear JSON
 
 const PORT = process.env.PORT||3003;
+app.listen(PORT,()=>{
+    console.log(`Servidor en funcionamiento en http://localhost:${PORT}`);
+});
 
 const users = []; //simula una bd
+const JWT_SECRET = process.env.JWT_SECRET
 
-app.post('/resgister',async(req,res)=>{
+app.post('/register',async(req,res)=>{
     const {username, password} = req.body;
     
     //Encriptar contraseña
     const hashedPassword = await bcrypt.hash(password,10);
-    const newUser = {username, pasword: hashedPassword};
+    const newUser = {username, password: hashedPassword};
     users.push(newUser);
     
     res.status(201).json({message: 'Usuario registrado exitosamente'});
 });
+
+
 
 
 //////////LOGIN/////////
@@ -33,7 +39,7 @@ app.post('/login',async(req,res)=>{
     }
 
     //Genera token JWT
-    const token = jwt.sing({ username: user.username}, process.env.JWT_SECRET,{
+    const token = jwt.sign({ username: user.username}, process.env.JWT_SECRET,{
         expiresIn:'1h'});
 
     res.json({token});
@@ -44,12 +50,14 @@ app.post('/login',async(req,res)=>{
 
 const authenticateToken = (req, res, next) =>{
     const authHeader = req.headers['authorization'];
-    const token = authHeader&&authHeader.split('')[1];
+    const token = authHeader&&authHeader.split(' ')[1];
 
     if(!token) return res.status(401).json({message: 'token requerido'});
 
-    jwt.verify(token,process.env.JWT_SECRET,(err,user)=>{
-        if(err) return res.stats(403).json({message: 'Token inválido'});
+    jwt.verify(token,JWT_SECRET,(err,user)=>{
+        if(err) return res.status(403).json({message: 'Token inválido'});
+        require.user = user;
+        next();
     });
 };
 
@@ -57,13 +65,10 @@ const authenticateToken = (req, res, next) =>{
 
  app.get('/protected', authenticateToken,(req, res)=>{
     res.json({message: 'Acceso autorizado', user: req.user});
+
 });
 
 
-////////INICIAR SERVIDOR/////////////
 
-app.listen(PORT,()=>{
-    console.log(`Servidor en funcionamiento en http://localhost:${PORT}`);
-});
 
 
